27fc4951623b036ef6b2cf7855868be77dfe1e41
Sebastian <rolla.sebastian1@gmail.com>
2024-01-04 10:19:43 +0100

    changed styles for nav and header and added js script for mobile nav

---
+++ js/mobileNav.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.nav__btn')
+const box = document.querySelector('.nav__links')
+const links = document.querySelectorAll('.nav__link')
+
+function mobileNav(){
+    box.classList.toggle('nav__active')
+}
+
+btn.addEventListener('click', mobileNav)
+links.forEach(item => {
+    item.addEventListener('click', mobileNav)
+})
